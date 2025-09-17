d561a2de100db887a1638e58c602f1f52c2435d8
diego riaño <diegoalejandroriano012@gmail.com>
2025-09-17 17:16:25 -0500

    cree la funcion que genera el nodo padre de la tarea con sus hijos usando js

--- js/main.js
+++ js/main.js
@@ -7,15 +7,36 @@ const completedList = document.getElementById('completed')
 
 //creamos la funcion que nos permite crear una nueva tarea a partir del formulario
 //toda etiqueta que vamos a crear es a partir de la maqueta html pre existente 
-function createToDoItem(){
-    //creamos el nodo o elemento padre
+function createToDoItem(textoItem){
+    //creamos el nodo o elemento padre o contenedor
     const item = document.createElement('div')
         item.classList.add('item-to-do') 
 
-    const checkbox = document.createElement('input')
+    const checkbox = document.createElement('input');
 
-    checkbox.type='checkbo'
-    // creamos el nodo hijo de input y le agregamos el type=checkbox'
+    checkbox.type='checkbox';
+    // creamos el nodo hijo de input y le agregamos el type=checkbox' es decir lo que escribe en el campo el usuario
+
+    //creamos el siguinete nodo hijo parrafo a este parrafo le asignoi el valor  del argumento de la funcion
+
+    const p = document.createElement('p');
+    p.textContent=textoItem;
+
+    //creamos el ultimo nodo hijo para el boton de eliminar 
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent='x';
+
+    //ensamblamos dentro del nodo padre sus nodos hijos, es decir la estructura de la tarea
+    item.appendChild(checkbox);
+    item.appendChild(p);
+    item.appendChild(deleteBtn);
+
+    //utilizamos el return para dar respuesta del elmento creado ya que lo utilizaremos mas adelante 
+
+    return item
+
+   
 
 
 
